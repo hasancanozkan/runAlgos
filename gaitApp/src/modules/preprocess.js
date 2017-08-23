5ca7c8944d271ccc52fb135effd7d751f7360059
prashant45 <prashant123219@gmail.com>
2017-08-23 12:58:28 +0200

    changed getRawData module in branch

--- gaitApp/src/modules/preprocess.js
+++ gaitApp/src/modules/preprocess.js
@@ -4,96 +4,38 @@
 //const fs = require('fs');
 import RNFetchBlob from 'react-native-fetch-blob';
 
-export async function getRawData(fileName: string, chunkSize: number) {
-    // Get raw Data and seperate coulmn-wise
-    // eslint-disable-next-line no-sync
-    //const x = fs.readFileSync(fileName);
-    //console.log('filename');
-    //console.log(fileName)
-    const x = RNFetchBlob.fs.readStream(fileName,'ascii',4096)
-        .then((stream) => {
-            let data = '';
-            stream.open();
-            stream.onData((chunk) => {
-                //const buffer = new ArrayBuffer(chunk.length);
-                //const Uint16View = new (chunk);
-                console.log(Uint16View);
-            });
+const base64ToUInt16 = function (base64D) {
+    // Decoding the base64 data
+    const binary_string =  window.atob(base64D);
+    const len = binary_string.length;
+    let uInt16D = [];
 
-            stream.onEnd(() => {
-                console.log('Unint16');
-                console.log();
-            })
-        })
-    /*let arrayBuffer = [];
-    x.open();
-    x.onData((chunk) =>{
-        const buffer = ArrayBuffer(chunk);
-        const UintView = new Uint16Array(buffer);
-        arrayBuffer.concat(UintView);
-    });
-    x.onEnd();
-    console.log('Preprocess : ');
-    console.log(arrayBuffer);*/
-
-
-    const arr = [];
-    const bool = true;
-
-    // Setting no-assert 'true' to allows offset to be beyond the end of buf
-    for (let i = 0, j = 0; i < x.length; i += 2) {
-        arr[j] = x.readUInt16LE(i, bool);
-        ++j;
+    /*
+    * Parsing the 16 bit hex into Int
+    * Here I know it should be little endian, hence first (i+1) + (i)*/
+    for (let i = 0, j = 0; i < len; i+=2, ++j) {
+        uInt16D[j] = parseInt(binary_string.charCodeAt(i+1).toString(16) + binary_string.charCodeAt(i).toString(16), 16);
     }
-
-    // Aligning [ AccX, AccY, AccZ, GyroX, GyroY, GyroZ from arr]
-    const R = [];
-
-    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
-        R.push(arr.slice(i, i + chunkSize));
-    }
-
-    // Rearranging the data
-    const finalArray = new Array(6);
-    for (let j = 0; j < 6; ++j) {
-        finalArray[j] = new Array(R.length);
-        for (let i = 0; i < R.length; ++i) {
-            finalArray[j][i] = R[i][j];
-        }
-    }
-
-    return finalArray;
-}
-/*
-module.exports.getRawData = function (fileName: string, chunkSize: number) {
-    // Get raw Data and seperate coulmn-wise
-    // eslint-disable-next-line no-sync
-    const x = fs.readFileSync(fileName);
-    const arr = [];
-    const bool = true;
-
-    // Setting no-assert 'true' to allows offset to be beyond the end of buf
-    for (let i = 0, j = 0; i < x.length; i += 2) {
-        arr[j] = x.readUInt16LE(i, bool);
-        ++j;
-    }
-
-    // Aligning [ AccX, AccY, AccZ, GyroX, GyroY, GyroZ from arr]
-    const R = [];
-
-    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
-        R.push(arr.slice(i, i + chunkSize));
-    }
-
-    // Rearranging the data
-    const finalArray = new Array(6);
-    for (let j = 0; j < 6; ++j) {
-        finalArray[j] = new Array(R.length);
-        for (let i = 0; i < R.length; ++i) {
-            finalArray[j][i] = R[i][j];
-        }
-    }
-
-    return finalArray;
+    return uInt16D;
 };
-*/
+
+export async function getRawData(fileName: string) {
+
+    let base64Data = await RNFetchBlob.fs.readFile(fileName, 'base64');
+    const uInt16Data = base64ToUInt16(base64Data);
+
+    let rawData = [[], [], [], [], [], []];
+
+    // Re-arranging the Data in the required form of [ AccX, AccY, AccZ, GyroX, GyroY, GyroZ ]
+    for (let i = 0; i < uInt16Data.length; i+=6) {
+        rawData[0].push(uInt16Data[i]);
+        rawData[1].push(uInt16Data[i+1]);
+        rawData[2].push(uInt16Data[i+2]);
+        rawData[3].push(uInt16Data[i+3]);
+        rawData[4].push(uInt16Data[i+4]);
+        rawData[5].push(uInt16Data[i+5]);
+    }
+
+
+    return rawData;
+}
